35ea981bdd601b22ed97345b68deb79c0dc8f27f
nrlefty5 <85917640+nrlefty5@users.noreply.github.com>
2026-03-11 20:42:03 -0400

    fix(ts): disable ignoreBuildErrors and ignoreDuringBuilds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,12 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Ignore lint/type errors during build so CI doesn't block deployment
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
 }
 
